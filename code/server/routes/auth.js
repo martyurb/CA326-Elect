483e1c081fbc8f77e252cb0fdd715e8bce1808d1
Martynas Urbanavicius <martynas@outlook.com>
2019-01-24 15:16:35 +0000

    Revert "more auth"
    This reverts commit d99296e028fd6388e90383fe6414784370e6a71e.

--- code/server/routes/auth.js
+++ code/server/routes/auth.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../models/User')
 
 
 /* GET home page. */
@@ -13,27 +12,10 @@ router.post('/login', function(req, res) {
     email = req.email;
     token = req.idToken;
     console.log(body)
-    User.findone({email=email},function(err,doc)){
-      if(err) {return res.status(500).send('error occured')}
-      else {
-        if(doc) {
-          return res.status(201).send('user already exsits')
-        }
-        else {
-          var record = new User()
-          record.email = email;
-          record.idToken = idToken;
-          record.save(fuction(err,user){
-            if(err){
-              return res.status(500).send('db error')
-            } else {
-              return res.send(user)
-            }
-          }
-        }
-      }
-    }
-  });
+    return res.status(200).json({
+      token: "test",
+      expiresIn: 45328,
+    });
 })
 
 
