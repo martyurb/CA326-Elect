d99296e028fd6388e90383fe6414784370e6a71e
Martynas Urbanavicius <martynas@outlook.com>
2019-01-24 15:16:35 +0000

    more auth

--- code/server/routes/auth.js
+++ code/server/routes/auth.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/User')
 
 
 /* GET home page. */
@@ -12,10 +13,27 @@ router.post('/login', function(req, res) {
     email = req.email;
     token = req.idToken;
     console.log(body)
-    return res.status(200).json({
-      token: "test",
-      expiresIn: 45328,
-    });
+    User.findone({email=email},function(err,doc)){
+      if(err) {return res.status(500).send('error occured')}
+      else {
+        if(doc) {
+          return res.status(201).send('user already exsits')
+        }
+        else {
+          var record = new User()
+          record.email = email;
+          record.idToken = idToken;
+          record.save(fuction(err,user){
+            if(err){
+              return res.status(500).send('db error')
+            } else {
+              return res.send(user)
+            }
+          }
+        }
+      }
+    }
+  });
 })
 
 
