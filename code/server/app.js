3070efc21c0c7bf86aa06305d1de70007f89d7ce
Martynas Urbanavicius <martynas.urbanavicius2@mail.dcu.ie>
2019-02-09 14:55:17 +0000

    add polls to app

--- code/server/app.js
+++ code/server/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var authRouter = require('./routes/auth');
+var pollRouter = require('./routes/polls');
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017');
