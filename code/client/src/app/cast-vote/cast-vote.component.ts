64bc39e0295de4841843bb9a248c7c43fc9da39a
groganj8 <jake.grogan8@mail.dcu.ie>
2019-02-20 19:52:28 +0000

    can encrypt and decrypt votes

--- code/client/src/app/cast-vote/cast-vote.component.ts
+++ code/client/src/app/cast-vote/cast-vote.component.ts
@@ -60,79 +60,84 @@ export class CastVoteComponent implements OnInit {
 
         };
 
-        const pubkey =
-  ['-----BEGIN PGP PUBLIC KEY BLOCK-----',
-  'Version: GnuPG v2.0.19 (GNU/Linux)',
-  '',
-  'mI0EUmEvTgEEANyWtQQMOybQ9JltDqmaX0WnNPJeLILIM36sw6zL0nfTQ5zXSS3+',
-  'fIF6P29lJFxpblWk02PSID5zX/DYU9/zjM2xPO8Oa4xo0cVTOTLj++Ri5mtr//f5',
-  'GLsIXxFrBJhD/ghFsL3Op0GXOeLJ9A5bsOn8th7x6JucNKuaRB6bQbSPABEBAAG0',
-  'JFRlc3QgTWNUZXN0aW5ndG9uIDx0ZXN0QGV4YW1wbGUuY29tPoi5BBMBAgAjBQJS',
-  'YS9OAhsvBwsJCAcDAgEGFQgCCQoLBBYCAwECHgECF4AACgkQSmNhOk1uQJQwDAP6',
-  'AgrTyqkRlJVqz2pb46TfbDM2TDF7o9CBnBzIGoxBhlRwpqALz7z2kxBDmwpQa+ki',
-  'Bq3jZN/UosY9y8bhwMAlnrDY9jP1gdCo+H0sD48CdXybblNwaYpwqC8VSpDdTndf',
-  '9j2wE/weihGp/DAdy/2kyBCaiOY1sjhUfJ1GogF49rC4jQRSYS9OAQQA6R/PtBFa',
-  'JaT4jq10yqASk4sqwVMsc6HcifM5lSdxzExFP74naUMMyEsKHP53QxTF0Grqusag',
-  'Qg/ZtgT0CN1HUM152y7ACOdp1giKjpMzOTQClqCoclyvWOFB+L/SwGEIJf7LSCEr',
-  'woBuJifJc8xAVr0XX0JthoW+uP91eTQ3XpsAEQEAAYkBPQQYAQIACQUCUmEvTgIb',
-  'LgCoCRBKY2E6TW5AlJ0gBBkBAgAGBQJSYS9OAAoJEOCE90RsICyXuqIEANmmiRCA',
-  'SF7YK7PvFkieJNwzeK0V3F2lGX+uu6Y3Q/Zxdtwc4xR+me/CSBmsURyXTO29OWhP',
-  'GLszPH9zSJU9BdDi6v0yNprmFPX/1Ng0Abn/sCkwetvjxC1YIvTLFwtUL/7v6NS2',
-  'bZpsUxRTg9+cSrMWWSNjiY9qUKajm1tuzPDZXAUEAMNmAN3xXN/Kjyvj2OK2ck0X',
-  'W748sl/tc3qiKPMJ+0AkMF7Pjhmh9nxqE9+QCEl7qinFqqBLjuzgUhBU4QlwX1GD',
-  'AtNTq6ihLMD5v1d82ZC7tNatdlDMGWnIdvEMCv2GZcuIqDQ9rXWs49e7tq1NncLY',
-  'hz3tYjKhoFTKEIq3y3Pp',
-  '=h/aX',
-  '-----END PGP PUBLIC KEY BLOCK-----'].join('\n');
+        const privkey =
+        ['-----BEGIN PGP PRIVATE KEY BLOCK-----',
+'Version: OpenPGP.js v4.4.7',
+'Comment: https://openpgpjs.org',
+'',
+'xcFGBFxtkuABBACi7n+76f9WXWHzB12vo1auFgKzOGwoIjvgH8gaTVgW2l3Y',
+'73vDmwXQ8tmRjFgcg1ASpCyqtPHOLywxuZ34UxpUUOvCrpOIcqMWgb7IWBGc',
+'TvWZEk0Jf0rWxAVcuJAPGSPb+ODu4iVb5Xy7tRcABfVE738Tl2+UkkgxfU8c',
+'BtLedwARAQAB/gkDCD6W/VdRqVDH4H6nqIMhgIMKdDUDJaD1EAEkjp8J2bQy',
+'omoTRVyCTXC+OK7wZHUaO+sSCdiHB+LdolmV4RN76hF2XPxq19UAZigzsZXi',
+'PVjSmkWpYri2AWbqsSGmY2cy8kApCuN3wPEPRjsdrQR7eCtI4Qs0NSfdvQFx',
+'7Kel9VC8HfKuJwhRYKOtmE3ZwvAauZeaJcY3mwAD1AZLUYuR2euYtpz21MsJ',
+'xPM8AHmoeX284+N0fkmgcwB3+WkE+EsHo/e5M6JGYUFvbfctfwrYWgJsqWJE',
+'1vQOv7dyDzlLEkA2HBDo8z9YsrizRnIdnKax126Mk/JdR8Txp9Vv2rJag9Gl',
+'LS9r6D7JFVzjebmyDLF/xWp+WYhseeoyCDHBNysoG1L/x7D4RaJcqSFEeShN',
+'SzG/PH4JWeFqxTuNpaQOmLEIWbFuIid+Xb0fvV3hZdH7WS2h+Csb157VgZ/B',
+'AqfUEpz6b7VAnfRjY0yJeIHuiLd/6/6kpSfNAMK1BBABCAAfBQJcbZLgBgsJ',
+'BwgDAgQVCAoCAxYCAQIZAQIbAwIeAQAKCRCtq+Andixrcy+aA/4sUm8rAWEV',
+'dNIInHXjaXMbe1YbYtvHmayM+0AbfAtEUBVMSvr0bgO0TVA2x5I+fkVO4vfG',
+'hwi5AivRbUizt8OEuUt/XvEo0d5TMhT8+mLzFwW9Mzt5LY7pLL//tuK3gLyL',
+'A1UULPIZQFDFs5Q4K/ELLyzcIows04P98/A1sVzV48fBRgRcbZLgAQP/TjGy',
+'LtvWG8XzHNLHwkv/rfTqlGb8fQ91pum0i/dudtYUss9XF5MsppMTGW/Ay6WF',
+'rbfJZWVlIDKm4EFtUHMsInzZe9LupofZFe5gJTvlYEW0T46Ik9WpJkDXQ8N7',
+'xKEEL2uBhjhj03oia2VAscG/lMm2VKOtBokbVCW/CtkSKqcAEQEAAf4JAwjP',
+'yM9Ww5cdAeAZ4i5zuJMrAE1TIivmG9lHihhURYU6bPpCNqRGjmjyaoxhbWA3',
+'6D3W18dsfVH3sdQ2Nm46NXtzHEb0ztW+54uPQ7TuK26GSezBnQBVhLYWb4Ag',
+'kc1Tx9eTTzgJR9GEG8XNshkIb3Q0K0C6Dz8U18vSZ3dCvsRxThKGXHg7M6zo',
+'ywJ3pX5O7zpukzBHb0Fj2wWgaVE4SvpiuyOUEwsCemonsbhDNTtTQ6EYgzBT',
+'s75QsP0JOMCC1uxLIiW+a8Ze2Mp+eFBAXjDgbdn83ej19d12RaYFQ8riqUOv',
+'CL7R2MfPPKOnhteJuNO7BvJRbv7xCrllpijaHhVB0gAKqv5K1tgz9pmKs65n',
+'616k987nOniBmNHFfYO/larGy0K6RA6sryEkFKm7NqYVgPG7ia+7b4sTCnEy',
+'o4I4YTMYTi+6NhG5B64JeESDb+UBdea5j3MQM0UsyOPBdEpPSEMt342aGzBr',
+'KjI+mD9+5cbq/o73wp8EGAEIAAkFAlxtkuACGwwACgkQravgJ3Ysa3PQTQP/',
+'TUR1rb3+7YvT/YsBA/ubyItmXAOaxXK8/9Ta93NNizfeNzh1ljQgcuJFoXKk',
+'Hcz1IFLxnXVOwTEI6TsC7mXI9chUgca9+KIqCTHF8bz+2IPVP0lx4PJcdhIn',
+'7+voq5X+4S8GFPGO4viyvX27OlaDjadbGHPDbKvJ1VIy/k6vD88=',
+'=OsQs/',
+'-----END PGP PRIVATE KEY BLOCK-----'].join('\r\n').replace(/\r/, '');
 
-const privkey =
-  ['-----BEGIN PGP PRIVATE KEY BLOCK-----',
-  'Version: GnuPG v2.0.19 (GNU/Linux)',
-  '',
-  'lQH+BFJhL04BBADclrUEDDsm0PSZbQ6pml9FpzTyXiyCyDN+rMOsy9J300Oc10kt',
-  '/nyBej9vZSRcaW5VpNNj0iA+c1/w2FPf84zNsTzvDmuMaNHFUzky4/vkYuZra//3',
-  '+Ri7CF8RawSYQ/4IRbC9zqdBlzniyfQOW7Dp/LYe8eibnDSrmkQem0G0jwARAQAB',
-  '/gMDAu7L//czBpE40p1ZqO8K3k7UejemjsQqc7kOqnlDYd1Z6/3NEA/UM30Siipr',
-  'KjdIFY5+hp0hcs6EiiNq0PDfm/W2j+7HfrZ5kpeQVxDek4irezYZrl7JS2xezaLv',
-  'k0Fv/6fxasnFtjOM6Qbstu67s5Gpl9y06ZxbP3VpT62+Xeibn/swWrfiJjuGEEhM',
-  'bgnsMpHtzAz/L8y6KSzViG/05hBaqrvk3/GeEA6nE+o0+0a6r0LYLTemmq6FbaA1',
-  'PHo+x7k7oFcBFUUeSzgx78GckuPwqr2mNfeF+IuSRnrlpZl3kcbHASPAOfEkyMXS',
-  'sWGE7grCAjbyQyM3OEXTSyqnehvGS/1RdB6kDDxGwgE/QFbwNyEh6K4eaaAThW2j',
-  'IEEI0WEnRkPi9fXyxhFsCLSI1XhqTaq7iDNqJTxE+AX2b9ZuZXAxI3Tc/7++vEyL',
-  '3p18N/MB2kt1Wb1azmXWL2EKlT1BZ5yDaJuBQ8BhphM3tCRUZXN0IE1jVGVzdGlu',
-  'Z3RvbiA8dGVzdEBleGFtcGxlLmNvbT6IuQQTAQIAIwUCUmEvTgIbLwcLCQgHAwIB',
-  'BhUIAgkKCwQWAgMBAh4BAheAAAoJEEpjYTpNbkCUMAwD+gIK08qpEZSVas9qW+Ok',
-  '32wzNkwxe6PQgZwcyBqMQYZUcKagC8+89pMQQ5sKUGvpIgat42Tf1KLGPcvG4cDA',
-  'JZ6w2PYz9YHQqPh9LA+PAnV8m25TcGmKcKgvFUqQ3U53X/Y9sBP8HooRqfwwHcv9',
-  'pMgQmojmNbI4VHydRqIBePawnQH+BFJhL04BBADpH8+0EVolpPiOrXTKoBKTiyrB',
-  'UyxzodyJ8zmVJ3HMTEU/vidpQwzISwoc/ndDFMXQauq6xqBCD9m2BPQI3UdQzXnb',
-  'LsAI52nWCIqOkzM5NAKWoKhyXK9Y4UH4v9LAYQgl/stIISvCgG4mJ8lzzEBWvRdf',
-  'Qm2Ghb64/3V5NDdemwARAQAB/gMDAu7L//czBpE40iPcpLzL7GwBbWFhSWgSLy53',
-  'Md99Kxw3cApWCok2E8R9/4VS0490xKZIa5y2I/K8thVhqk96Z8Kbt7MRMC1WLHgC',
-  'qJvkeQCI6PrFM0PUIPLHAQtDJYKtaLXxYuexcAdKzZj3FHdtLNWCooK6n3vJlL1c',
-  'WjZcHJ1PH7USlj1jup4XfxsbziuysRUSyXkjn92GZLm+64vCIiwhqAYoizF2NHHG',
-  'hRTN4gQzxrxgkeVchl+ag7DkQUDANIIVI+A63JeLJgWJiH1fbYlwESByHW+zBFNt',
-  'qStjfIOhjrfNIc3RvsggbDdWQLcbxmLZj4sB0ydPSgRKoaUdRHJY0S4vp9ouKOtl',
-  '2au/P1BP3bhD0fDXl91oeheYth+MSmsJFDg/vZJzCJhFaQ9dp+2EnjN5auNCNbaI',
-  'beFJRHFf9cha8p3hh+AK54NRCT++B2MXYf+TPwqX88jYMBv8kk8vYUgo8128r1zQ',
-  'EzjviQE9BBgBAgAJBQJSYS9OAhsuAKgJEEpjYTpNbkCUnSAEGQECAAYFAlJhL04A',
-  'CgkQ4IT3RGwgLJe6ogQA2aaJEIBIXtgrs+8WSJ4k3DN4rRXcXaUZf667pjdD9nF2',
-  '3BzjFH6Z78JIGaxRHJdM7b05aE8YuzM8f3NIlT0F0OLq/TI2muYU9f/U2DQBuf+w',
-  'KTB62+PELVgi9MsXC1Qv/u/o1LZtmmxTFFOD35xKsxZZI2OJj2pQpqObW27M8Nlc',
-  'BQQAw2YA3fFc38qPK+PY4rZyTRdbvjyyX+1zeqIo8wn7QCQwXs+OGaH2fGoT35AI',
-  'SXuqKcWqoEuO7OBSEFThCXBfUYMC01OrqKEswPm/V3zZkLu01q12UMwZach28QwK',
-  '/YZly4ioND2tdazj17u2rU2dwtiHPe1iMqGgVMoQirfLc+k=',
-  '=lw5e',
-  '-----END PGP PRIVATE KEY BLOCK-----'].join('\n');
+const pubkey = ['-----BEGIN PGP PUBLIC KEY BLOCK-----',
+'Version: OpenPGP.js v4.4.7',
+'Comment: https://openpgpjs.org',
+'',
+'xo0EXG2S4AEEAKLuf7vp/1ZdYfMHXa+jVq4WArM4bCgiO+AfyBpNWBbaXdjv',
+'e8ObBdDy2ZGMWByDUBKkLKq08c4vLDG5nfhTGlRQ68Kuk4hyoxaBvshYEZxO',
+'9ZkSTQl/StbEBVy4kA8ZI9v44O7iJVvlfLu1FwAF9UTvfxOXb5SSSDF9TxwG',
+'0t53ABEBAAHNAMK1BBABCAAfBQJcbZLgBgsJBwgDAgQVCAoCAxYCAQIZAQIb',
+'AwIeAQAKCRCtq+Andixrcy+aA/4sUm8rAWEVdNIInHXjaXMbe1YbYtvHmayM',
+'+0AbfAtEUBVMSvr0bgO0TVA2x5I+fkVO4vfGhwi5AivRbUizt8OEuUt/XvEo',
+'0d5TMhT8+mLzFwW9Mzt5LY7pLL//tuK3gLyLA1UULPIZQFDFs5Q4K/ELLyzc',
+'Iows04P98/A1sVzV486NBFxtkuABA/9OMbIu29YbxfMc0sfCS/+t9OqUZvx9',
+'D3Wm6bSL92521hSyz1cXkyymkxMZb8DLpYWtt8llZWUgMqbgQW1QcywifNl7',
+'0u6mh9kV7mAlO+VgRbRPjoiT1akmQNdDw3vEoQQva4GGOGPTeiJrZUCxwb+U',
+'ybZUo60GiRtUJb8K2RIqpwARAQABwp8EGAEIAAkFAlxtkuACGwwACgkQravg',
+'J3Ysa3PQTQP/TUR1rb3+7YvT/YsBA/ubyItmXAOaxXK8/9Ta93NNizfeNzh1',
+'ljQgcuJFoXKkHcz1IFLxnXVOwTEI6TsC7mXI9chUgca9+KIqCTHF8bz+2IPV',
+'P0lx4PJcdhIn7+voq5X+4S8GFPGO4viyvX27OlaDjadbGHPDbKvJ1VIy/k6v',
+'D88=',
+'=HZmp',
+'-----END PGP PUBLIC KEY BLOCK-----'].join('\r\n');
+
+        const privKeyObj = ( pgp.key.readArmored(privkey));
+        privKeyObj.keys[0].decrypt('oiwerl43ksmpoq5wieurxmzcvnb9843lj3459ks');
 
         const options = {
           data: voteObject.option,       // input as Message object
-          publicKeys: (pgp.key.readArmored(pubkey)).keys, // for encryption
+          publicKeys: pgp.key.readArmored(pubkey).keys, // for encryption
         };
 
         const pgpmsg = pgp.encrypt(options).then(function(ciphertext) {
-          console.log(ciphertext.data);
-          });
+          const encrypted = ciphertext.data;
+          pgp.decrypt({
+            message: pgp.message.readArmored(encrypted),
+            privateKey: privKeyObj.keys[0],
+            publicKeys: pgp.key.readArmored(pubkey).keys,
+          }).then((decrypted) => { console.log(decrypted); });
+        });
+
       }
 
     }
