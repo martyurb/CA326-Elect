da1218d9ddceb9ed46f342a7609349068228bc34
Martynas Urbanavicius <martynas.urbanavicius2@mail.dcu.ie>
2019-02-25 17:20:53 +0000

    Added Sample line chart to statistics page

--- code/client/src/app/statistics/statistics.component.ts
+++ code/client/src/app/statistics/statistics.component.ts
@@ -8,6 +8,28 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./statistics.component.css']
 })
 export class StatisticsComponent implements OnInit {
+  // lineChart
+  public lineChartType:string = 'line';
+  public lineChartData:Array<any> = [
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
+    {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
+  ];
+  public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartOptions:any = {
+    responsive: true
+  };
+
+  // events
+  public chartClicked(e:any):void {
+    console.log(e);
+  }
+
+  public chartHovered(e:any):void {
+    console.log(e);
+  }
+
+
 
   constructor(private route: ActivatedRoute,
               private router: Router,
