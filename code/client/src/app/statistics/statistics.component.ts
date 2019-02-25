59385c764db8a4ba528dddffe4813d3977d70a9d
Martynas Urbanavicius <martynas.urbanavicius2@mail.dcu.ie>
2019-02-25 19:15:17 +0000

    Lint

--- code/client/src/app/statistics/statistics.component.ts
+++ code/client/src/app/statistics/statistics.component.ts
@@ -9,23 +9,23 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class StatisticsComponent implements OnInit {
   // lineChart
-  public lineChartType:string = 'line';
-  public lineChartData:Array<any> = [
+  public lineChartType = 'line';
+  public lineChartData :Array<any> = [
     {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
     {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
     {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'},
   ];
-  public lineChartLabels:Array<any> = ['0', '1', '2', '3', '4', '5', '6', '7'];
-  public lineChartOptions:any = {
+  public lineChartLabels :Array<any> = ['0', '1', '2', '3', '4', '5', '6', '7'];
+  public lineChartOptions :any = {
     responsive: true
   };
 
   // events
-  public chartClicked(e:any):void {
+  public chartClicked(e:any) :void {
     console.log(e);
   }
 
-  public chartHovered(e:any):void {
+  public chartHovered(e:any) :void {
     console.log(e);
   }
 
@@ -44,9 +44,9 @@ export class StatisticsComponent implements OnInit {
         }
       });
     this.authService.getStatsLine(pollid).subscribe((response) => {
-        console.log(response);
+
         this.lineChartData = response;
-        console.log("here",this.lineChartData);
+
       });
 
 
